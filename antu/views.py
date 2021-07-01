50cc2b2a0ab907aa07d888c794cfa4a6aefe1849
nilldiggonto <nilldiggonto@gmail.com>
2021-07-01 22:39:48 +0600

    Static folder

--- antu/views.py
+++ antu/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def homePage(request):
+    template_name = 'base.html'
+    return render(request,template_name)
--- moinul_port/settings.py
+++ moinul_port/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'antu',
 ]
 
 MIDDLEWARE = [
@@ -43,7 +44,7 @@ ROOT_URLCONF = 'moinul_port.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
