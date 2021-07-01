67c3e6de489b25132b6ea9ee3bc7d7334aef7471
nilldiggonto <nilldiggonto@gmail.com>
2021-07-02 00:33:35 +0600

    work with about section

--- antu/models.py
+++ antu/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class ProfileModel(models.Model):
+    name        =   models.CharField(max_length=14)
+    position    =   models.CharField(max_length=30)
+    github      =   models.CharField(max_length=120)
+    linkd       =   models.CharField(max_length=120)
+    insta       =   models.CharField(max_length=120)
+    details     =   models.TextField()
+    personal_info   = models.TextField(null=True,blank=True)
+
+    picture     =   models.ImageField(upload_to='antu/profile/',blank=True,null=True)
+    picture_two =   models.ImageField(upload_to='antu/profile/',blank=True,null=True)
+    resume      = models.FileField(upload_to ='antu/resume/',blank=True,null=True)
+    profile_code    = models.CharField(max_length=4,unique=True)
+
+    def __str__(self):
+        return self.name
--- antu/views.py
+++ antu/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from .models import ProfileModel
 # Create your views here.
 def homePage(request):
     template_name = 'base.html'
-    return render(request,template_name)
+    profile_info = get_object_or_404(ProfileModel,profile_code='1234')
+
+    context = {
+        'profile_info':profile_info,
+    }
+    return render(request,template_name,context)
