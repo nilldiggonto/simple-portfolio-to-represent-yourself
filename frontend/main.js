dbbe7591a0a67525259e15c8af1d49330fd52ce3
nilldiggonto <nilldiggonto@gmail.com>
2021-07-07 01:03:22 +0600

    Frontend BS5

---
+++ frontend/main.js
@@ -0,0 +1,14 @@
+
+$(window).scroll(function(){
+
+    if($(window).width() > 990) {
+                $('.navbar').addClass('fixed-top');
+               
+            }
+    var scroll = $(window).scrollTop();
+    if(scroll < 60){
+        $('.fixed-top').css('background', 'transparent');
+    } else{
+        $('.fixed-top').css('background', 'white').css('border-bottom','1px solid grey');
+    }
+});
--- moinul_port/settings.py
+++ moinul_port/settings.py
@@ -123,8 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [BASE_DIR / 'moinul_port/static']
+STATIC_ROOT = Path(BASE_DIR,'static/')
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
